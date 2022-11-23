b6477bf3af36bea5108bc7acf6b42350c96fe21e
Moises.alves <Moises.alves@atp.com.br>
2022-11-23 18:02:34 -0300

    Adicionados comentários

--- src/ExemploEntidade.java
+++ src/ExemploEntidade.java
@@ -1,32 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList; //Importa package ArrayList 
+import java.util.List; //Importa package List
 
-public class ExemploEntidade{
-  public static void main (String args[]){    
-	  Retangulo ret = new Retangulo();
+public class ExemploEntidade{ //Cria classe, nesse caso, com o nome ExemploEntidade
+  public static void main (String args[]){    //Classe Main, onde o código roda
+	  Retangulo ret = new Retangulo();  //Instanciado o objeto Retangulo e referenciando a variavel RET
 	  
-	  ret.setAltura(20.0);
-	  ret.setBase(10.0);
+	  ret.setAltura(20.0);  //Método que define o valor de Altura dentro do objeto Retangulo armazenado na variavel ret
+	  ret.setBase(10.0); //Método que define o valor da Base dentro do objeto Retangulo armazenado na variavel ret
 	  
-	  Triangulo tri = new Triangulo();
+	  Triangulo tri = new Triangulo(); //Instanciado o objeto Triangulo e referenciando a variavel TRI
 	  
-	  tri.setAltura(10.0);
-	  tri.setBase(10.0);
+	  tri.setAltura(10.0); //Método que define o valor de Altura dentro do objeto Triangulo armazenado na variavel tri
+	  tri.setBase(10.0); //Método que define o valor da Base dentro do objeto Triangulo armazenado na variavel tri
 	  
-	  Circulo cir = new Circulo();
+	  Circulo cir = new Circulo(); //Instanciado o objeto Circulo e referenciando a variavel CIR
 	  
-	  cir.setRaio(36.0);
+	  cir.setRaio(36.0); //Método que define o valor do raio dentro do objeto Circulo armazenado na variavel cir
 	  
 	// Adiciona as figuras geometricas em uma lista 
-	    List<FiguraGeometrica> listaFigurasGeometricas = new ArrayList<>() ; 
-	    listaFigurasGeometricas.add( ret );    
-	    listaFigurasGeometricas.add( tri ); 
-	    listaFigurasGeometricas.add( cir ); 
+	    List<FiguraGeometrica> listaFigurasGeometricas = new ArrayList<>() ; //Cria uma lista de Objetos do tipo FiguraGeometrica e armazena na variável listaFigurasGeometricas (não sabemos até então o número de items)
+	    listaFigurasGeometricas.add( ret );    //Adiciona a Lista, o objeto Retangulo armazenado na variável ret
+	    listaFigurasGeometricas.add( tri );    //Adiciona a Lista, o objeto Triangulo armazenado na variável tri
+	    listaFigurasGeometricas.add( cir );    //Adiciona a Lista, o objeto Circulo armazenado na variável cir
 
 	// Mostra dados das figuras geometricas 
-	   for(FiguraGeometrica figuraGeometrica : listaFigurasGeometricas) { 
-	       System.out.println( figuraGeometrica.getDescricao() ); 
-	       System.out.printf( "Area da figura: %.2f \n", figuraGeometrica.getArea() ); 
+	   for(FiguraGeometrica figuraGeometrica : listaFigurasGeometricas) {  //Loop for é executado para cada item em listaFigurasGeometricas (nesse caso, 3 vezes)
+	       System.out.println( figuraGeometrica.getDescricao() );  //imprime na tela a descrição retornada pelo método definido dentro do objeto que estiver sendo acessado (um por vez)
+	       System.out.printf( "Area da figura: %.2f \n", figuraGeometrica.getArea() );  //imprime na tela o valor da area retornada pelo metodo definido na classe
 	   } 
   } 
 }
+
--- src/FiguraGeometrica.java
+++ src/FiguraGeometrica.java
@@ -1,52 +1,52 @@
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica { //Cria classe abstrata FiguraGeometrica que será sobreposta pelas classes Filhas
 
-	private String nome;
+	private String nome; // cria variável nome dentro da classe
 	
 	
-	public abstract double getArea();
+	public abstract double getArea(); // cria classe metodo abstrato que será sobreposto pelas classes filhas
 	
-	public String getNome(){
+	public String getNome(){  //cria metodo que retorna o valor armazenado na variável nome
 		return nome;
 	}
 	
-	public void setNome(String nome){
+	public void setNome(String nome){ //cria metodo que atribui o valor à variável nome dentro da classe
 		this.nome = nome;
 	}
 	
-	public String getDescricao() {
+	public String getDescricao() { //Cria metodo para retornar a descrição, nesse caso, ela chama a função getNome, retornando o nome da FiguraGeometrica
 		String texto = getNome();
 		return texto;
 	}
 
 }
 
-class Retangulo extends FiguraGeometrica {
+class Retangulo extends FiguraGeometrica { //Cria classe Retangulo que herda de FiguraGeometrica e sobrepoe os metodos para funções específicas dessa classe filha
 	
-	private double base;
-	private double altura;
+	private double base;  // cria variável base dentro da classe
+	private double altura; // cria variável altura dentro da classe
 	
-	public double getBase(){
+	public double getBase(){ //cria metodo que retorna o valor armazenado na variável base
 		return base;
 	}
 	
-	public void setBase(Double base){
+	public void setBase(Double base){ //cria metodo que atribui o valor à variável base dentro da classe
 		this.base = base;
 	}
 	
-	public double getAltura(){
+	public double getAltura(){ //cria metodo que retorna o valor armazenado na variável altura
 		return altura;
 	}
 	
-	public void setAltura(Double altura){
+	public void setAltura(Double altura){  //cria metodo que atribui o valor à variável altura dentro da classe
 		this.altura = altura;
 	}
 	
-	public double getArea() {
+	public double getArea() { //cria metodo que retorna o valor do cálculo da área para um Retangulo (base * altura)
 		double area = base * altura;
 		return area;
 	}
 	
-	public String getDescricao() {
+	public String getDescricao() { //Cria metodo que retorna uma string concatenada contendo o tipo de Figura, sua base e sua altura
 		String texto = "Retangulo: Base = "+getBase()+", Altura = "+getAltura();
 		return texto;
 	}
@@ -54,33 +54,33 @@ class Retangulo extends FiguraGeometrica {
 	
 }
 
-class Triangulo extends FiguraGeometrica {
+class Triangulo extends FiguraGeometrica { //Cria classe Triangulo que herda de FiguraGeometrica e sobrepoe os metodos para funções específicas dessa classe filha
 	
-	private double base;
-	private double altura;
+	private double base;// cria variável base dentro da classe
+	private double altura;// cria variável altura dentro da classe
 	
-	public double getBase(){
+	public double getBase(){//cria metodo que retorna o valor armazenado na variável base
 		return base;
 	}
 	
-	public void setBase(Double base){
+	public void setBase(Double base){ //cria metodo que atribui o valor à variável base dentro da classe
 		this.base = base;
 	}
 	
-	public double getAltura(){
+	public double getAltura(){//cria metodo que retorna o valor armazenado na variável altura
 		return altura;
 	}
 	
-	public void setAltura(Double altura){
+	public void setAltura(Double altura){ //cria metodo que atribui o valor à variável altura dentro da classe
 		this.altura = altura;
 	}
 	
-	public double getArea() {
+	public double getArea() {  //cria metodo que retorna o valor do cálculo da área para um Triangulo (base * altura)/2
 		double area = (base * altura)/2;
 		return area;
 	}
 	
-	public String getDescricao() {
+	public String getDescricao() {  //Cria metodo que retorna uma string concatenada contendo o tipo de Figura, sua base e sua altura
 		String texto = "Triangulo: Base = "+getBase()+", Altura = "+getAltura();
 		return texto;
 	}
@@ -88,25 +88,25 @@ class Triangulo extends FiguraGeometrica {
 	
 }
 
-class Circulo extends FiguraGeometrica {
+class Circulo extends FiguraGeometrica {//Cria classe Circulo que herda de FiguraGeometrica e sobrepoe os metodos para funções específicas dessa classe filha
 	
-	private double raio;
+	private double raio; // cria variável raio dentro da classe
 	
-	public double getRaio(){
+	public double getRaio(){ //cria metodo que retorna o valor armazenado na variável raio
 		return raio;
 	}
 	
-	public void setRaio(Double raio){
+	public void setRaio(Double raio){ //cria metodo que atribui o valor à variável raio dentro da classe
 		this.raio = raio;
 	}
 	
 	
-	public double getArea() {
+	public double getArea() { //cria metodo que retorna o valor do cálculo da área para um Circulo (pi * raio * raio), pi = 3.14
 		double area = 3.14 * raio * raio;
 		return area;
 	}
 	
-	public String getDescricao() {
+	public String getDescricao() { //Cria metodo que retorna uma string concatenada contendo o tipo de Figura, sua base e sua altura
 		String texto = "Circulo: Raio = "+getRaio();
 		return texto;
 	}
